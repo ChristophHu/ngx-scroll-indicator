f76d62ec591a166ddbb8af8d22f72c5a2b6896ee
christophhu <christoph.huschenhoefer@gmx.de>
2025-06-11 14:33:56 +0200

    add indicator show logic

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { ViewportScroller } from '@angular/common';
-import { Component, ElementRef, HostListener } from '@angular/core';
+import { AsyncPipe, ViewportScroller } from '@angular/common';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
 
@@ -12,11 +12,14 @@ export interface Section {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    AsyncPipe,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   private readonly _sections = new BehaviorSubject<Section[]>([])
   sections$: Observable<any> = this._sections.asObservable()
@@ -24,17 +27,40 @@ export class AppComponent {
   private _sectionsAhead: Section[] = []
   private _currentSection: Section = { title: '', activated: false, fragment: '', offsetTop: 0 }
 
+  private readonly _scrollIndicator = new BehaviorSubject<boolean>(false)
+  scrollIndicator$: Observable<boolean> = this._scrollIndicator.asObservable()
+  
+  private readonly _scrollToTop = new BehaviorSubject<boolean>(false)
+  scrollToTop$: Observable<boolean> = this._scrollToTop.asObservable()
+
   constructor(private readonly _viewportScroller: ViewportScroller, public el: ElementRef<HTMLElement>) { }
   
-  @HostListener('window:scroll', ['$event']) onWindowScroll(event: any) {
-    let currentScrollPosition = this._viewportScroller.getScrollPosition()[1]
-    this._sectionsAhead = this._sections.value.filter(page => page.offsetTop >= currentScrollPosition)
-    this._currentSection = this._sectionsAhead[0]
-    this._sections.value.forEach(page => page.activated = page.fragment === this._currentSection.fragment)
+  ngOnInit(): void {
+    console.log('windowheight', window.innerHeight)
+    console.log('scrollposition', this._viewportScroller.getScrollPosition()[1])
+    console.log('document', document.documentElement.scrollHeight)
+    this._scrollIndicator.next(this._viewportScroller.getScrollPosition()[1] + window.innerHeight < document.documentElement.scrollHeight)
+    this._scrollToTop.next(this._viewportScroller.getScrollPosition()[1] + window.innerHeight < document.documentElement.scrollHeight)
+    this.scrollIndicator$.subscribe((visible: boolean) => {
+      console.log('Scroll indicator visibility:', visible)
+    })
+    this.scrollToTop$.subscribe((visible: boolean) => {
+      console.log('Scroll to top:', visible)
+    })
   }
 
-  @HostListener('window:resize') public detectResize(): void {
-    console.log(this.el.nativeElement.offsetWidth)
-    console.log(this.el.nativeElement.offsetHeight)
+  @HostListener('window:scroll', ['$event']) onWindowScroll(event: any) {
+    // let currentScrollPosition = this._viewportScroller.getScrollPosition()[1]
+    // this._sectionsAhead = this._sections.value.filter(page => page.offsetTop >= currentScrollPosition)
+    // this._currentSection = this._sectionsAhead[0]
+    // this._sections.value.forEach(page => page.activated = page.fragment === this._currentSection.fragment)
+    console.log(this._viewportScroller.getScrollPosition()[1] + window.innerHeight < document.documentElement.scrollHeight)
+  }
+  // add a hostlistener for window resize to detect if the scroll indicator is visible
+  @HostListener('window:resize', ['$event']) onWindowResize(event: any) {
+    console.log('windowheight', window.innerHeight)
+    console.log('scrollposition', this._viewportScroller.getScrollPosition()[1])
+    console.log('document', document.documentElement.scrollHeight)
+    this._scrollIndicator.next(this._viewportScroller.getScrollPosition()[1] + window.innerHeight < document.documentElement.scrollHeight)
   }
 }
